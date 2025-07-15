3f302dba3b008312f1b5bf5923916666ba58002a
Ondroidd <zigooff@gmail.com>
2025-07-15 21:10:41 +0200

    added asteroid field

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
 
 def main():
 
-    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+
     dt = 0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
+    
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+
+    Player.containers = (drawable, updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
+
     while True:
 
         # enable close button
@@ -22,15 +41,15 @@ def main():
                 return
 
         screen.fill(color="black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
+        # FPS limit = 60
         dt = clock.tick(60) / 1000
-
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
